471d6d19d0956034c37be4c4b3d3cdbc23b4ac27
Marvin Petersen <39294105+m-petersen@users.noreply.github.com>
2023-03-08 14:30:18 +0100

    Add files via upload

--- 04_abannotate_pls.m
+++ 04_abannotate_pls.m
@@ -1,6 +1,6 @@
 sources_table = abannotate_get_sources;
 
-parcellated_data_path = '/home/marvin/mount/hdd8tb/CSI_UKB/2023_mets_combined_cohorts_continuous/output/pls/pls_ukb_bootstrap_schaefer100_df.csv'
+parcellated_data_path = 'pls_ukb_bootstrap_schaefer100_df.csv'
 parcellated_data = csvread(parcellated_data_path);
 
 opt.n_nulls = 1000;
